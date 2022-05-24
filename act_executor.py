4b4b58358d01393d79c4f542fd3c25d9faa2d81e
lukasabrie <lukasabrie@gmail.com>
2022-05-24 04:49:51 +0200

    Testing outerr

--- act_executor.py
+++ act_executor.py
@@ -10,9 +10,10 @@ def execute_act_prsim_code(act_file, prsim_file):
         print(outp)
     p = subprocess.Popen(["prsim", "test_inv.prs"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     temp = p.communicate(input=outp.encode())
+    outstr, outerr = temp
     print(temp)
-    outstr = temp[0].decode()
-    outerr = temp[2].decode()
+    #outstr = temp[0].decode()
+    #outerr = temp[2].decode()
     p.kill()
     print(outstr)
     return outstr, outerr
